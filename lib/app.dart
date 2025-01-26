d629b2d32a57c365832690af66c2581079bfaa04
Riyam224 <cs50riyam221@gmail.com>
2025-01-26 22:50:24 +0300

    modify the font family

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:random_clean/core/utils/app_colors.dart';
 import 'package:random_clean/core/utils/app_strings.dart';
-import 'package:random_clean/core/utils/hex_color.dart';
-
 import 'config/themes/app_theme.dart';
 
 class QuoteApp extends StatelessWidget {
@@ -15,8 +12,10 @@ class QuoteApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //! theme: ThemeData(primaryColor: HexColor('#032254')),
       theme: appTheme(),
-      home: const Center(
-        child: Text('Quote app'),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Quote app'),
+        ),
       ),
     );
   }
