7c31965f92dc375d459f8863738d55f2ad938580
Riyam224 <cs50riyam221@gmail.com>
2025-01-26 23:04:03 +0300

    add images to the app

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:random_clean/core/utils/app_strings.dart';
+import 'package:random_clean/core/utils/assets.dart';
 import 'config/themes/app_theme.dart';
 
 class QuoteApp extends StatelessWidget {
@@ -12,11 +13,10 @@ class QuoteApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //! theme: ThemeData(primaryColor: HexColor('#032254')),
       theme: appTheme(),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Quote app'),
-        ),
-      ),
+      home: Scaffold(
+          body: Image.asset(
+        Assets.imagesQuote,
+      )),
     );
   }
 }
---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesQuote
+  /// assets/images/quote.png
+  static const String imagesQuote = "assets/images/quote.png";
+}
